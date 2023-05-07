ce814e867193e1562c6003b8df160788d8df3188
miyus <dnguyen84@go.pasadena.edu>
2023-05-07 11:43:16 -0700

    Add project files.

---
+++ main.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+#include "../blackjack.h"
+
+using namespace std;
+
+int main()
+{
+    Menu playMenu;
+    cout << "Let's play BlackJack!";
+
+    while (true) {
+        playMenu.StartGame();
+        int playerValue = 0;
+        int dealerValue = 0;
+        int choice = 0;
+
+        while (playerValue < 21 && dealerValue < 21 && choice != 3) {
+            playMenu.MakeSelection();
+            playerValue = playMenu.RetPlayerValue();
+            dealerValue = playMenu.RetDealerValue();
+            choice = playMenu.RetChoice();
+        }
+
+        playMenu.DetermineWinner();
+        cout << "Play again?\n";
+    }
+
+    return 0;
+}
